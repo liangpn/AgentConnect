960a57334e3f2d8c256520ff87c756f3eb1c4e49
changshan <chgaowei@gmail.com>
2024-11-13 13:13:50 +0800

    add negotiate demo files

---
+++ examples/simple_node/negotiation_node_bob.py
@@ -0,0 +1,8 @@
+# AgentConnect: https://github.com/chgaowei/AgentConnect
+# Author: GaoWei Chang
+# Email: chgaowei@gmail.com
+# Website: https://agent-network-protocol.com/
+#
+# This project is open-sourced under the MIT License. For details, please see the LICENSE file.
+
+
